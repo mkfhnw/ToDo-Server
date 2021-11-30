03a4f6665ab7a7c7a5afa92d22eaea628b41c810
mkfhnw <margareta.karaqi@students.fhnw.ch>
2021-11-30 17:57:52 +0100

    Added methods to ToDoClient

--- src/client/ToDoClient.java
+++ src/client/ToDoClient.java
@@ -1,6 +1,7 @@
 package client;
 
 import common.Message;
+import common.MessageType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -86,6 +87,117 @@ public class ToDoClient {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
+
+     // Will be used to know which kind of MessageType is needed
+        public void sendMessageType(MessageType messageType) {
+        	
+        	switch (messageType) {
+            
+            case LOGIN:
+            	login();
+            	break;
+            case LOGOUT:
+            	logout();
+            	break;
+            case CREATE_LOGIN:
+            	createLogin();
+            	break;
+            case CREATE_TODO:
+            	createToDo();
+            	break;
+            case CHANGE_PASSWORD:
+            	changePassword();
+            	break;
+            case GET_TODO:
+            	getToDo();
+            	break;
+            case DELETE_TODO:
+            	deleteToDo();
+            	break;
+            case LIST_TODOS:
+            	listToDos();
+            	break;
+            case PING:
+            	ping();
+            }
+            
+        }
+        
+        public void login() {
+        	ToDoClient toDoClient = new ToDoClient();
+            ArrayList<String> data = new ArrayList<>();
+        	
+        	toDoClient.sendMessage("LOGIN", data);
+        	data.add("Username");
+        	data.add("Password");
+        	
+        	// Token
+        }
+        
+        public void logout() {
+        	
+        	// Token must be invalid
+        	
+        	// DELETE toDoClient.sendMessage("LOGOUT", data);
+        }
+        
+        
+        public void createLogin() {
+        	// Username
+        	// Password
+        	// (Lastname, Surname)
+        	// (Password validation)
+        	// (Token)
+        	
+        	// toDoClient.sendMessage("CREATE_LOGIN", data);
+        }
+        
+        public void createToDo() {
+        	// title (+ validation)
+        	// description (+ validation)
+        	// duedate (with and without)
+        	// Priority
+        	// Token-Validation
+        	
+        	// toDoClient.sendMessage("CREATE_TODO", data);
+        }
+
+        public void changePassword() {
+        	// password
+        	// (password validation)
+        	// (Token)
+        	// Validation (Password length + Token valid)
+        	
+        	// toDoClient.sendMessage("CHANGE_PASSWORD", data);
+        }
+        
+        public void getToDo() {
+        	// ID
+        	// Token-Validation
+        	
+        	// toDoClient.sendMessage("GET_TODO", data);
+        }
+        
+        public void deleteToDo() {
+        	// getToDo();
+        	// clear(); / remove();
+        	// Token-Validation
+        	
+        	// toDoClient.sendMessage("DELETE_TODO", data);
+        }
+        
+        public void listToDos() {
+        	// ArrayList data
+        	// Token-Validation
+        	
+        	// toDoClient.sendMessage("LIST_TODOS", data);
+        }
+        
+        public void ping() {
+        	// Token
+        	
+        	// toDoClient.sendMessage("PING", data);
+        }
+    
 }
