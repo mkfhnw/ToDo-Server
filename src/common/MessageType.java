1329e6cfa4ff7553ac498b66767ee150e1b1fd1c
MadMowgli <laeubin.j@gmail.com>
2021-12-10 18:09:56 +0100

    Overrode the toString method of the MessageType enum

--- src/common/MessageType.java
+++ src/common/MessageType.java
@@ -3,17 +3,30 @@ package common;
 public enum MessageType {
 
     // The following values are valid for client-to-server messages
-    LOGIN,
-    LOGOUT,
-    CREATE_LOGIN,
-    CREATE_TODO,
-    CHANGE_PASSWORD,
-    GET_TODO,
-    DELETE_TODO,
-    LIST_TODOS,
-    PING,
+    LOGIN("Login"),
+    LOGOUT("Logout"),
+    CREATE_LOGIN("Create_Login"),
+    CREATE_TODO("Create_Todo"),
+    CHANGE_PASSWORD("Change_Password"),
+    GET_TODO("Get_ToDo"),
+    DELETE_TODO("Delete_ToDo"),
+    LIST_TODOS("List_ToDos"),
+    PING("Ping"),
 
     // The following value is valid for server-to-client messages
-    RESULT;
+    RESULT("Result");
+
+    // Fields of the enum values
+    private String name;
+
+    // Constructor to pass the names
+    MessageType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 
 }
